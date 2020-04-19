0a9354ba817b8942cd290a8331b2745b22c01263
eddie-coding <adityabahuguna007@gmail.com>
2020-04-19 22:23:26 +0530

    Modifying Admin Service files to include functionality for Labs

--- medical/src/main/java/devops/medical/service/AdminService.java
+++ medical/src/main/java/devops/medical/service/AdminService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import devops.medical.model.Admin;
 import devops.medical.model.AdminLogin;
 import devops.medical.model.Doctor;
+import devops.medical.model.Lab;
 import devops.medical.model.Patient;
 public interface AdminService {
 	
@@ -22,6 +23,21 @@ public interface AdminService {
 	int updateDoctor(Doctor doctor);
 	 
 	int deleteDoctor(String id);
+	
+	List<Lab> getAllLabs();
+	
+	Lab validateLab(Lab lab);
+	
+	int registerLab(Lab lab);
+	
+	Lab getLab(String id);
+	
+	int updateLab(Lab lab);
+	
+	int deleteLab(String id);
+	
+	
+	
 	
 
 }
--- medical/src/main/java/devops/medical/service/AdminServiceImpl.java
+++ medical/src/main/java/devops/medical/service/AdminServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import devops.medical.model.Admin;
 import devops.medical.model.AdminLogin;
 import devops.medical.model.Doctor;
+import devops.medical.model.Lab;
 import devops.medical.model.Patient;
 import devops.medical.dao.AdminDao;
 
@@ -42,6 +43,36 @@ public class AdminServiceImpl implements AdminService{
 	public int deleteDoctor(String id) {
 		return AdminDao.deleteDoctor(id);
 	}
+	
+	
+	
+	public List<Lab> getAllLabs(){
+		return AdminDao.getAllLabs();
+	}
+	
+	public Lab validateLab(Lab lab) {
+		return AdminDao.validateLab(lab);
+	}
+	
+	public int registerLab(Lab lab) {
+		return AdminDao.registerLab(lab);
+	}
+	
+	public Lab getLab(String id) {
+		return AdminDao.getLab(id);
+	}
+	
+	public int updateLab(Lab lab) {
+		return AdminDao.updateLab(lab);
+	}
+	
+	public int deleteLab(String id) {
+		return AdminDao.deleteLab(id);
+	}
+	
+	
+	
+	
 
 	
 
