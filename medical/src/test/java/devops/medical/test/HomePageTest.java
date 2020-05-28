ce5a02423638c7b2ee58602842a3fd046b2260f1
Shobhit Joshi <a.captainjoshi@gmail.com>
2020-05-28 09:13:09 +0530

    Html Unit Driver

--- medical/src/test/java/devops/medical/test/HomePageTest.java
+++ medical/src/test/java/devops/medical/test/HomePageTest.java
@@ -7,6 +7,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;	
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;	
 
 public class HomePageTest {
 	
@@ -17,26 +20,26 @@ public class HomePageTest {
 	
 	@BeforeClass
 	public void testSetUp() {
-		driver = new ChromeDriver();
+		driver = new HtmlUnitDriver();
 	}
 	
 	@Test
 	public void verifyHomePageTitle() {
-		driver.navigate().to(appURL1);
+		driver.get(appURL1);
 		String getTitle = driver.getTitle();
 		Assert.assertEquals(getTitle, "Welcome");
 	}
 	
 	@Test
 	public void verifyPatientLogin() {
-		driver.navigate().to(appURL2);
+		driver.get(appURL2);
 		String getTitle = driver.getTitle();
 		Assert.assertEquals(getTitle, "Patient Login");
 	}
 	
 	@Test
 	public void verifyPatientRegistration() {
-		driver.navigate().to(appURL3);
+		driver.get(appURL3);
 		String getTitle = driver.getTitle();
 		Assert.assertEquals(getTitle, "Registration");
 	}
